0a8d560f1381615630dce9031c412effeaad1dcb
Adriano Santos <sleipnir@bsd.com.br>
2019-11-04 18:01:45 -0300

    Formatted messages

--- cloudstate/src/commands.rs
+++ cloudstate/src/commands.rs
@@ -105,7 +105,6 @@ pub mod command {
         println!("{0: <10} | {1: <20} | {2: <10}", "Profile", "Dependencies", "Resolved");
         for (profile, dependencies) in &profiles {
             println!("{0: <10} | {1: <20} | {2: <10}", profile, dependencies, resolve_dependencies(profile));
-            //println!("[{}]:[{}]:[{}]", profile, dependencies, resolve_dependencies(profile));
         }
     }
 
