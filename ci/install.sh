fa69ba7ae0c3b5ead25c6395c07fcd67c23d3d5e
Adriano Santos <sleipnir@bsd.com.br>
2019-11-21 16:10:04 -0300

    Remove uninstall

--- ci/install.sh
+++ ci/install.sh
@@ -6,7 +6,7 @@ set -ex
 
 install_c_toolchain() {
     case $TARGET in
-        aarch64-unknown-linux-gnu)
+        arch64-unknown-linux-gnu)
             sudo apt-get install -y --no-install-recommends \
                  gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
             ;;
