d78e29633717be2f8c4e6d579d1fa1f9a37d5792
Jonathan Nagy <nagytech@users.noreply.github.com>
2019-11-23 19:45:12 +1100

    Unix cross-platform home directory

--- bin/cloudstate-installer.sh
+++ bin/cloudstate-installer.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-HOME_DIR=$( getent passwd "$USER" | cut -d: -f6 )
+
+if [ $HOME ]; then
+  HOME_DIR=$HOME
+elif [ $( command -v getent ) ]; then
+  HOME_DIR=$( getent passwd "$USER" | cut -d: -f6 )
+else
+  echo "Could not determine path to user's home directory"
+  exit 1
+fi
 
 #TODO: Extract function
 echo $OSTYPE
@@ -19,7 +27,10 @@ echo $OSTYPE
 #        # Unknown.
 #fi
 
-mkdir -p $HOME_DIR/.cloudstate
+CLOUDSTATE_CONF=$HOME_DIR/.cloudstate
+if [ ! -d "$CLOUDSTATE_CONF" ]; then
+    mkdir -p "$CLOUDSTATE_CONF"
+fi
 
 #TODO: Extract function
 # Verify Docker
