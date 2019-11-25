981ab19f6eca8fa46da1fea9fd05171cf0075eaf
Adriano Santos <sleipnir@bsd.com.br>
2019-11-25 18:50:52 -0300

    Add multiplataform support

--- bin/cloudstate-installer.sh
+++ bin/cloudstate-installer.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+VERSION=0.3.26
+DARWIN_RELEASE_URL=https://github.com/sleipnir/cloudstate-cli/releases/download/$VERSION/cloudstate-cli-$VERSION-x86_64-apple-darwin.tar.gz
+LINUX_RELEASE_URL=https://github.com/sleipnir/cloudstate-cli/releases/download/$VERSION/cloudstate-cli-$VERSION-x86_64-unknown-linux-gnu.tar.gz
+
+RELEASE_URL=$LINUX_RELEASE_URL
 
 if [ $HOME ]; then
   HOME_DIR=$HOME
@@ -26,12 +31,15 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OSX
   if [ $( command -v brew ) ]; then
+    RELEASE_URL=$DARWIN_RELEASE_URL
+
     BREW_PREFIX=$( brew --prefix )
     BASH_COMPLETION_DIR=$BREW_PREFIX/etc/bash_completion.d
     if [[ ! -d "$BASH_COMPLETION_DIR" ]]; then
       skip_bash_completion "Could not determine the path to bash_completion.d.  Skipping install of completion tools"
     fi
   fi
+
 #elif [[ "$OSTYPE" == "cygwin" ]]; then
 #        # POSIX compatibility layer and Linux environment emulation for Windows
 #elif [[ "$OSTYPE" == "msys" ]]; then
@@ -89,52 +97,15 @@ if [[ $(which curl) && $(curl --version) ]]; then
     # command
 fi
 
-#TODO: Extract function
-# Verify languages dependencies
-
-# Verify Java
-if [[ $(which java) && $(java -version) ]]; then
-    echo "Update java"
-    # command
-  else
-    echo "Install java"
-    # command
-fi
-
-# Verify Maven
-if [[ $(which mvn) && $(mvn --version) ]]; then
-    echo "Update mvn"
-    # command
-  else
-    echo "Install mvn"
-    # command
-fi
-
-# Verify Rust
-if [[ $(which rustc) && $(rustc --version) ]]; then
-    echo "Update rustc"
-    # command
-  else
-    echo "Install rustc"
-    # command
-fi
-
-if [[ $(which cargo) && $(cargo --version) ]]; then
-    echo "Update cargo"
-    # command
-  else
-    echo "Install cargo"
-    # command
-fi
-
-# And many others dependencies validations here:
-#......
-
 # Download binary
 touch /tmp/cloudstate
-curl -vvv -H 'Cache-Control: no-cache' --url https://raw.githubusercontent.com/sleipnir/cloudstate-cli/master/bin/linux-x86/cloudstate --output /tmp/cloudstate
-mv /tmp/cloudstate /usr/local/bin
+#curl -vvv -H 'Cache-Control: no-cache' --url https://raw.githubusercontent.com/sleipnir/cloudstate-cli/master/bin/linux-x86/cloudstate --output /tmp/cloudstate
+curl -vvv -H 'Cache-Control: no-cache' --url $RELEASE_URL --output /tmp/cloudstate.tar.gz
+
+tar -zxvf /tmp/cloudstate.tar.gz --directory /usr/local/bin
+#mv /tmp/cloudstate /usr/local/bin
 chmod +x /usr/local/bin/cloudstate
+rm -rf /tmp/cloudstate.tar.gz
 
 # Install completions
 cloudstate completions bash >> $BASH_COMPLETION_DIR/cloudstate.bash-completion
