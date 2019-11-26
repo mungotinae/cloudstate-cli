c21eb3920cb59293cb12a72e2c22469f4d10fab1
Adriano Santos <sleipnir@bsd.com.br>
2019-11-26 10:58:10 -0300

    Fix

--- ci/install.sh
+++ ci/install.sh
@@ -12,7 +12,7 @@ install_c_toolchain() {
             ;;
         x86_64-unknown-linux-musl)
             sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-                 pkg-config libssl-dev \
+                 pkg-config libssl-dev && \
             ls  -ltr /usr/lib/*libssl*
             ;;
         *)
