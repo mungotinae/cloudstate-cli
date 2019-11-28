d7f39c92e2f18155b1bd5d803e6e1cf23218d6bf
Adriano Santos <sleipnir@bsd.com.br>
2019-11-28 11:04:36 -0300

    Add node support

--- cloudstate/src/builders/node.rs
+++ cloudstate/src/builders/node.rs
@@ -14,26 +14,67 @@ impl ProjectBuilder for NodeBuilder {
     }
 
     fn pre_compile(&self, app: &Application) {
-        unimplemented!()
+        env::set_current_dir(&app.work_dir);
+
+        // Find and replace occurrences of {docker-image} and {tag} in deployment.yml
+        set_deployment_vars(&app);
+
+        // set dockerfile
+        let docker_path = Path::new(&app.work_dir).join("Dockerfile");
+        let docker_template_content = fs::read_to_string(docker_path.clone()).unwrap();
+
+        let dockerfile = docker_template_content.replace("{application-name}", app.name.as_ref());
+
+        let mut docker_file = File::create(docker_path).unwrap();
+        docker_file.write_all(dockerfile.as_ref());
     }
 
     fn compile(&self, app: &Application) {
-        unimplemented!()
-    }
+        env::set_current_dir(&app.work_dir);
 
-    fn pos_compile(&self, app: &Application) {
-        unimplemented!()
+        println!("{} Downloading and install dependencies...", Emojis::default().floppy_disk());
+        println!("{} Compiling project...", Emojis::default().coffee());
+        let status = Command::new("npm")
+            .arg("install")
+            .status();
+
+        if status.is_ok() {
+            println!("{} Project successfully compiled", Emojis::default().ok())
+        }
     }
 
     fn build(self, app: Application) {
-        unimplemented!()
+        println!("{} Building Project...", Emojis::default().worker());
+        env::set_current_dir(&app.work_dir);
+        self.compile(&app);
+
+        let status = Command::new("docker")
+            .arg("build")
+            .arg("-t")
+            .arg(format!("{}:{}", app.registry, app.tag))
+            .arg(&app.work_dir)
+            .status();
+
+        if status.is_ok() {
+            println!("{} Image created successfully!", Emojis::default().frame_picture())
+        }
     }
 
     fn push(self, app: Application) {
-        unimplemented!()
+        env::set_current_dir(&app.work_dir);
+
+        println!("{} Push container image...", Emojis::default().rocket());
+        let status = Command::new("docker")
+            .arg("push")
+            .arg(format!("{}:{}", app.registry, app.tag))
+            .status();
+
+        if status.is_ok() {
+            println!("{} Pushed!", Emojis::default().moon());
+        }
     }
 
     fn deploy(self, app: Application) {
-        unimplemented!()
+        k8s_deploy(&app)
     }
 }
